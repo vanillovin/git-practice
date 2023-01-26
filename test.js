09477ddf991b7cbb6eb1833dba3c9e403ca09dc5
vanillovin <70941696+vanillovin@users.noreply.github.com>
2023-01-26 12:51:50 +0900

    New branch (#3)
    * Add getName
    
    * Update getName

---
+++ test.js
@@ -0,0 +1,20 @@
+export const sumbitHandler = async (evt) => {
+  evt.preventDefault();
+  if (newTodo) {
+    await postTodos({ todo: newTodo })
+      .then((response) => {
+        dispatch({ type: 'ADD', todo: response });
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+    inputRef.current.value = '';
+    inputRef.current.focus();
+    setNewTodo('');
+  }
+};
+
+export const getName = name => {
+  if (!name) throw new Error('name is required');
+  return name;
+};
