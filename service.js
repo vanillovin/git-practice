3c8f493c5c402ec7b432715600fc9af34b0ab7c6
vanillovin <vanillovin@gmail.com>
2023-02-01 23:30:20 +0900

    clean

--- service.js
+++
@@ -1,7 +0,0 @@
-class LoginService {
-  constructor() {
-    
-  }
-}
-
-class CherryService {}
--- test.js
+++
@@ -1,4 +0,0 @@
-export const getName = name => {
-  if (!name) throw new Error('name is required');
-  return name;
-};
